9108ca61fa1f3507bfc6974afd117dd0a4f6431c
xianing151 <3918789296@qq.com>
2025-08-06 17:31:46 +0800

    Update SNhub.lua

--- SNhub.lua
+++ SNhub.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xianing151/SNHUB/main/SN...hub.lua"))()
+game:GetService("StarterGui"):SetCore("SendNotification",{ Title = "SNHUB"; Text ="因处于特殊时段，卡密系统暂时关闭，拒绝辱华服务器"; Duration = 10; })
