1b827b42cca22e88058321f8f6feeab4ebfd0bfe
xianing151 <3918789296@qq.com>
2025-08-06 13:59:29 +0800

    Update yui.lua

--- yui.lua
+++ yui.lua
@@ -1,5 +1,5 @@
 while true do 
   local args = {"1"} 
   game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("Screen"):WaitForChild("Sack UI"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
-  wait(0.01)
+  wait(0.00001)
 end
